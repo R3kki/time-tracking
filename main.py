d542beee8da208777faec6680019a02de1f1e089
r3kki <richie.parkie@gmail.com>
2020-01-15 00:24:36 -0500

    collects foreground program name and time interval

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from win32gui import GetWindowText, GetForegroundWindow
 import time
+import re
 import psutil
 
-for i in range(5):
-    time.sleep(2)
-    print(GetWindowText(GetForegroundWindow()))
-    print(GetForegroundWindow())
-    ++i
+TIME_INTERVAL = 2
+RUNNING = 1
+DAILY = 86400
+
+data = {}
+
+
+def collect_data(duration):
+    prev_program = ''
+    timeout = duration
+    if duration <= 0:
+        timeout = DAILY
+    for i in range(timeout):
+        time.sleep(TIME_INTERVAL)
+        raw_window_info = GetWindowText(GetForegroundWindow())
+        curr_program = (re.split(' - ', raw_window_info))[-1]
+        if prev_program in data:
+            data[prev_program] += TIME_INTERVAL
+        elif prev_program != '':
+            data[prev_program] = TIME_INTERVAL
+        prev_program = curr_program
+        ++i
+        print(data)
+
+
+# test with 6 seconds of running
+collect_data(6)
+
 
 
